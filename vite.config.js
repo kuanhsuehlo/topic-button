7672cdb92026ee52d983d2f39670ec85bdcd5445
kuanhsuehlo <woody40408@gmail.com>
2026-02-02 18:11:08 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/topic-button/' : '/',
   plugins: [react()],
 })
